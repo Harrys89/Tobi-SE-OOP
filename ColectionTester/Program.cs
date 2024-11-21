1c82cdc68e7fe289fc95cbc868320eb220ba6130
Harrys89 <168172282+Harrys89@users.noreply.github.com>
2024-11-21 15:59:34 +0100

    Added basic implementation for own int typ

---
+++ ColectionTester/Program.cs
@@ -0,0 +1,9 @@
+﻿using MyCollections;
+
+var MyList = new MyList();
+
+MyList.Add(1);
+MyList.Add(2);
+MyList.Add(3);
+
+System.Console.WriteLine(MyList.Get(1));
---
+++ MyCollections/MyList.cs
@@ -0,0 +1,53 @@
+﻿namespace MyCollections;
+
+internal class Node
+{
+    public int Value;
+    public Node? Next;
+    public Node(int value)
+    {
+        Value = value;
+        Next = null;
+    }
+}
+
+public class MyList
+{
+    private Node? head;
+
+    public void Add(int value)
+    {
+        if (head is null)
+        {
+            head = new Node(value);
+        }
+        else
+        {
+            Node current = head;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+            current.Next = new Node(value);
+        }
+    }
+
+    public int Get (int index)
+    {
+        int count = 0;
+        Node current = head;
+
+        while (current != null)
+        {
+            if (index ==count)
+            {
+               return current.Value;
+            }
+
+            count++;
+            current = current.Next;
+        }
+
+        throw new IndexOutOfRangeException();
+    }
+}
